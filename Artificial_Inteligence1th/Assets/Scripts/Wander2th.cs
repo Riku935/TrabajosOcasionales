506b0341d043252c772af4b23987ce3c571c24e3
unknown <exodo302.richy@gmail.com>
2023-03-09 11:57:04 -0600

    AI

--- Artificial_Inteligence1th/Assets/Scripts/Wander2th.cs
+++ Artificial_Inteligence1th/Assets/Scripts/Wander2th.cs
@@ -1,68 +1,143 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wander2th : MonoBehaviour
 {
-    public float CircleRadius = 1;
-    public float TurnChance = 0.05f;
-    public float MaxRadius = 5;
+    [Header("Wander")]
+    [SerializeField] private float circleDistance;
+    [SerializeField] private float circleRadius;
+    [SerializeField] private float angleChange;
+    [SerializeField] private Vector3 targetChange;
+    [SerializeField] private GameObject targetObjectRandom;
+    private Vector3 targetWander;
+    private float angleWander;
+    private bool coroutineRunning;
+    private Queue<Vector3> targetQueue = new Queue<Vector3>();
 
-    public float Mass = 15;
-    public float MaxSpeed = 3;
-    public float MaxForce = 15;
+    [Header("Seek")]
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private Transform target;
 
+    [Header("Run Away")]
+    [SerializeField] private float speedAway;
+    [SerializeField] private float maxSpeedAway;
+
+    private Vector3 steering;
     private Vector3 velocity;
-    private Vector3 wanderForce;
-    private Vector3 target;
+
+
+    #region Action
+    [SerializeField] private SteeringType _type;
+    private Dictionary<string, Action> _actions = new Dictionary<string, Action>();
+    private enum SteeringType
+    {
+        Seek, RunAway, Wander
+    }
+    #endregion
 
     private void Start()
     {
-        velocity = Random.onUnitSphere;
-        wanderForce = GetRandomWanderForce();
+        targetWander = transform.position;
+        _actions.Add("Seek", CalculateSeek);
+        _actions.Add("RunAway", CalculateRunAway);
+        _actions.Add("Wander", CalculateWander);
+        StartCoroutine(ChangeTarget());
+        StartCoroutine(ChangeAngle());
+    }
+    void Update()
+    {
+        if (_actions.TryGetValue(_type.ToString(), out Action action))
+        {
+            action();
+        }
     }
 
-    private void Update()
+    private void CalculateSeek()
     {
-        var desiredVelocity = GetWanderForce();
-        desiredVelocity = desiredVelocity.normalized * MaxSpeed;
-
-        var steeringForce = desiredVelocity - velocity;
-        steeringForce = Vector3.ClampMagnitude(steeringForce, MaxForce);
-        steeringForce /= Mass;
-
-        velocity = Vector3.ClampMagnitude(velocity + steeringForce, MaxSpeed);
+        Vector3 steering = Seek(target.position);
+        Move(steering);
+    }
+    #region Seek
+    Vector3 Seek(Vector3 target)
+    {
+        Vector3 desiredVelocity = ((target - transform.position).normalized * speed);
+        return desiredVelocity - velocity;
+    }
+    private void Move(Vector3 steering)
+    {
+        velocity += steering;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
         transform.position += velocity * Time.deltaTime;
-        transform.forward = velocity.normalized;
-
-        Debug.DrawRay(transform.position, velocity.normalized * 2, Color.green);
-        Debug.DrawRay(transform.position, desiredVelocity.normalized * 2, Color.magenta);
     }
-
-    private Vector3 GetWanderForce()
+    #endregion 
+    private void CalculateRunAway()
     {
-        if (transform.position.magnitude > MaxRadius)
-        {
-            var directionToCenter = (target - transform.position).normalized;
-            wanderForce = velocity.normalized + directionToCenter;
-        }
-        else if (Random.value < TurnChance)
-        {
-            wanderForce = GetRandomWanderForce();
-        }
+        Vector3 steering = Seek(target.position);
+        Move(steering * -1);
+    }
+    private void CalculateWander()
+    {
+        Vector3 newTarget = targetWander;
+        Vector3 steering = Seek(wonderForce() + Seek(targetWander));
+        Move(steering);
+        Vector3 circleCenter = velocity.normalized * circleDistance;
+        Vector3 circlePosition = transform.position + circleCenter;
+        Debug.DrawLine(circlePosition, circlePosition + GetDisplacement(), Color.red); //Displacement
+        Debug.DrawLine(transform.position, transform.position + wonderForce(), Color.blue); //WanderForce
+    }
+    #region WanderFunctions
+    private Vector3 GetCircle()
+    {
+        Vector3 circleCenter = velocity.normalized * circleDistance;
+        Debug.DrawLine(transform.position, transform.position + circleCenter, Color.green);
+        return circleCenter;
+    }
+    private Vector3 GetDisplacement()
+    {
+        Vector3 displacement = Vector3.up * circleRadius;
+        Quaternion rotate = Quaternion.AngleAxis(angleWander, Vector3.forward);
+        displacement = rotate * displacement;
+        return displacement;
 
+    }
+    private Vector3 wonderForce()
+    {
+        var wanderForce = GetCircle() + GetDisplacement();
         return wanderForce;
     }
 
-    private Vector3 GetRandomWanderForce()
+    private IEnumerator ChangeAngle()
     {
-        var circleCenter = velocity.normalized;
-        var randomPoint = Random.insideUnitCircle;
-
-        var displacement = new Vector3(randomPoint.x, randomPoint.y) * CircleRadius;
-        displacement = Quaternion.LookRotation(velocity) * displacement;
-
-        var wanderForce = circleCenter + displacement;
-        return wanderForce;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            angleWander = UnityEngine.Random.Range(-90, 90);
+        }
     }
+    #endregion
+    #region RandomTarget
+    private void FillQueue()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 randomTarget = new Vector3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f), 0);
+            targetQueue.Enqueue(randomTarget);
+        }
+    }
+    IEnumerator ChangeTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            if (targetQueue.Count == 0) FillQueue();
+            targetWander = targetQueue.Dequeue();
+            targetChange = targetWander;
+            targetObjectRandom.transform.position = targetWander;
+        }
+    }
+    #endregion
 }
+
