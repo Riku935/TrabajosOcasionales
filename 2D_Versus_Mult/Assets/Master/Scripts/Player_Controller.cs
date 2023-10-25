f6c840982052b2506e8836966cdaa849ef3f4baa
Riku935 <samuelgtz025@gmail.com>
2023-10-24 18:42:32 -0600

    Muerte

--- 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
+++ 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player_Controller : MonoBehaviour
+public class Player_Controller : FSM
 {
+    [Header("Player_Settings")]
     public float velocidad = 5.0f;
+    [SerializeField] float JumpForce;
+    [SerializeField] float attackRadius;
+    [SerializeField] bool isplayerDead;
+    public enum FSMState
+    {
+        None, Idle, Moving, Jump, Fall, Attack, Hit, Dead
+    }
+    public FSMState curState = FSMState.None;
 
     void Update()
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoX = movimientoHorizontal * velocidad * Time.deltaTime;
         transform.Translate(new Vector3(movimientoX, 0, 0));
+    }
+    private void FixedUpdate()
+    {
+        
     }
 }
