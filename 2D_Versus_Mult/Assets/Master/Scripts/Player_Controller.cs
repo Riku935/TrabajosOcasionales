734b39eba530f1bf47d72d5fc328857d3a6667e2
Riku935 <exodo302.richy@gmail.com>
2023-11-07 18:47:43 -0600

    Siu

--- 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
+++ 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
@@ -2,28 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using Mirror;
 public class Player_Controller : FSM
 {
     [Header("Player_Settings")]
-    public float velocidad = 5.0f;
+    [SerializeField] float velocidad = 5.0f;
     [SerializeField] float JumpForce;
     [SerializeField] float attackRadius;
     [SerializeField] bool isplayerDead;
-    public enum FSMState
+    [SerializeField] int playerLife;
+
+    private Animator anim;
+    private SpriteRenderer spr;
+
+    private void Start()
     {
-        None, Idle, Moving, Jump, Fall, Attack, Hit, Dead
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
     }
-    public FSMState curState = FSMState.None;
+
 
     void Update()
+    {
+        Walk();
+        Flip();
+        Death();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    private void Flip() 
+    {
+        if (Input.GetAxis("Horizontal") >= 0.1)
+        {
+            spr.flipX = false;
+        }
+        if (Input.GetAxis("Horizontal") <= -0.1)
+        {
+            spr.flipX = true;
+        }
+    }
+
+    private void Walk()
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoX = movimientoHorizontal * velocidad * Time.deltaTime;
         transform.Translate(new Vector3(movimientoX, 0, 0));
+        anim.SetFloat("Horizontal", Mathf.Abs(movimientoHorizontal));
     }
-    private void FixedUpdate()
+    private void Death()
     {
-        
+        if (playerLife == 0)
+        {
+            velocidad = 0;
+
+        }
     }
 }
